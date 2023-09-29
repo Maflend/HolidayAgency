97911a6677195b7ce10cec2a7ad20a58913d5037
Maksim Vorobev <maks233online@gmail.com>
2023-09-29 21:20:54 +0300

    HA-2 Пайплайн для FluentValidation. Валидатор создания заказа

---
+++ src/Core/Application/HA.Application/Common/Behaviors/ValidationPiplineBehavior.cs
@@ -0,0 +1,49 @@
+﻿using FluentResults;
+using FluentValidation;
+using HA.Application.Common.Models.Errors;
+using MediatR;
+
+namespace HA.Application.Common.Behaviors;
+
+public class ValidationPiplineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TResponse : ResultBase, new()
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPiplineBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var errorsDictionary = _validators
+            .Select(v => v.Validate(request))
+            .SelectMany(result => result.Errors)
+            .Where(error => error != null)
+            .GroupBy(
+                x => x.PropertyName,
+                x => x.ErrorMessage,
+                (propertyName, errorMessages) => new
+                {
+                    Key = propertyName,
+                    Values = errorMessages.Distinct().ToArray()
+                })
+            .ToDictionary(x => x.Key, x => x.Values);
+
+        if (errorsDictionary.Any())
+        {
+            var result = new TResponse();
+            result.Reasons.Add(new ValidationError(errorsDictionary));
+
+            return result;
+        }
+
+        return await next();
+    }
+}
