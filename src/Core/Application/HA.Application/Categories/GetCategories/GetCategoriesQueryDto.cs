584fa0c56a4898dac87e97c45941f1f4773eb39c
Тимур Муталимов <atimasa21@outlook.com>
2023-10-03 01:22:44 +0300

    H-5 Реализовал логику получения всех категорий. Документация.

---
+++ src/Core/Application/HA.Application/Categories/GetCategories/GetCategoriesQueryDto.cs
@@ -0,0 +1,23 @@
+﻿namespace HA.Application.Categories.GetCategories;
+
+/// <summary>
+/// Информация о категории.
+/// </summary>
+public class GetCategoriesQueryDto
+{
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Цена за час.
+    /// </summary>
+    public decimal PriceOfHourse { get; set; }
+}
+
---
+++ src/Core/Application/HA.Application/Categories/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using FluentResults;
+using HA.Application.Common.Persistence;
+using HA.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HA.Application.Categories.GetCategories;
+
+/// <summary>
+/// Обработчик запроса на получение категорий.
+/// </summary>
+public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, Result<List<GetCategoriesQueryDto>>>
+{
+    private readonly IApplicationDbContext _dbcontext;
+
+    public GetCategoriesQueryHandler(IApplicationDbContext dbContext)
+    {
+        _dbcontext = dbContext;
+    }
+    
+    public async Task<Result<List<GetCategoriesQueryDto>>> Handle(
+        GetCategoriesQuery request,
+        CancellationToken cancellationToken) =>
+        await _dbcontext.Categories
+        .Select(o => Map(o))
+        .ToListAsync(cancellationToken);
+
+    private GetCategoriesQueryDto Map(Category category) => new()
+    {
+        Id = category.Id,
+        Name = category.Name,
+        PriceOfHourse = category.PriceOfHourse,
+    };
+}
+
