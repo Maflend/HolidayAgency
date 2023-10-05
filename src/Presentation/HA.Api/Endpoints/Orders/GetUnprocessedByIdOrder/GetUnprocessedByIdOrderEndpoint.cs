e3c3511292a511fb4c5e0627a347508ad3045b68
Тимур Муталимов <atimasa21@outlook.com>
2023-10-05 22:01:25 +0300

    H-7 Микро изменение убрал {} у namespace

--- src/Presentation/HA.Api/Endpoints/Orders/GetUnprocessedByIdOrder/GetUnprocessedByIdOrderEndpoint.cs
+++ src/Presentation/HA.Api/Endpoints/Orders/GetUnprocessedByIdOrder/GetUnprocessedByIdOrderEndpoint.cs
@@ -1,31 +1,29 @@
-﻿using HA.Application.Orders.GetUnprocessedOrders.Response;
-using HA.Application.Orders.GetUnprocessedOrders;
+﻿using FluentResults.Extensions.AspNetCore;
+using HA.Application.Orders.GetUnprocessedOrderById;
+using HA.Application.Orders.GetUnprocessedOrders.Response;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using FluentResults.Extensions.AspNetCore;
-using HA.Application.Orders.GetUnprocessedOrderById;
 
-namespace HA.Api.Endpoints.Orders.GetUnprocessedByIdOrders
+namespace HA.Api.Endpoints.Orders.GetUnprocessedByIdOrders;
+public static class GetUnprocessedByIdOrderEndpoint
 {
-    public static class GetUnprocessedByIdOrderEndpoint
+    public static void MapGetUnprocessedOrdersByIdEndpoint(this RouteGroupBuilder group)
     {
-        public static void MapGetUnprocessedOrdersByIdEndpoint(this RouteGroupBuilder group)
-        {
-            group.MapGet("{id}", GetUnprocessedOrdersAsync)
-                .Produces(200, typeof(List<GetUnprocessedOrderListDto>))
-                .WithOpenApi(opts =>
-                {
-                    opts.Summary = "Получить необработанные заказы по идентификатору.";
-                    opts.Description = "Получить необработанные по идентификатору.";
+        group.MapGet("{id}", GetUnprocessedOrdersAsync)
+            .Produces(200, typeof(List<GetUnprocessedOrderListDto>))
+            .WithOpenApi(opts =>
+            {
+                opts.Summary = "Получить необработанные заказы по идентификатору.";
+                opts.Description = "Получить необработанные по идентификатору.";
 
-                    return opts;
-                });
-        }
+                return opts;
+            });
+    }
 
-        internal static Task<ActionResult> GetUnprocessedOrdersAsync([FromRoute]Guid id,
-            ISender sender)
-        {
-            return sender.Send(new GetUnprocessedOrderByIdQuery(id)).ToActionResult();
-        }
+    internal static Task<ActionResult> GetUnprocessedOrdersAsync([FromRoute] Guid id,
+        ISender sender)
+    {
+        return sender.Send(new GetUnprocessedOrderByIdQuery(id)).ToActionResult();
     }
 }
+
