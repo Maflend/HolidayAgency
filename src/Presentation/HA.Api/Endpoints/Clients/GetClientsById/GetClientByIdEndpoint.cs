2fe5f58dd10c9064606ee2642022bef36ec0f3c5
Тимур Муталимов <atimasa21@outlook.com>
2023-10-07 20:06:08 +0300

    H-10 Добавил Endpoint для Клиента

---
+++ src/Presentation/HA.Api/Endpoints/Clients/GetClientsById/GetClientByIdEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults.Extensions.AspNetCore;
+using HA.Application.Clients.GetClientById;
+using HA.Application.Orders.GetUnprocessedOrders.Response;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HA.Api.Endpoints.Clients.GetClientsById;
+
+public static class GetClientByIdEndpoint
+{
+    public static void MapGetClientByIdEndpoint(this RouteGroupBuilder group)
+    {
+        group.MapGet("{id}", GetClientByIdAsync)
+            .Produces(200, typeof(GetUnprocessedOrderListDto))
+            .WithOpenApi(opts =>
+            {
+                opts.Summary = "Получить клиента по идентификатору.";
+                opts.Description = "Получить клиента по идентификатору.";
+
+                return opts;
+            });
+    }
+
+    internal static Task<ActionResult> GetClientByIdAsync(
+        [FromRoute] Guid id,
+        ISender sender)
+    {
+        return sender.Send(new GetClientByIdQuery(id)).ToActionResult();
+    }
+}
+
